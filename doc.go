952d5c84b3e321d8c6991faf5e1d9301a4fdc4d0
thediveo <hidden@hidden.org>
2020-09-17 10:14:30 +0200

    fixes package documentation

--- doc.go
+++ doc.go
@@ -6,6 +6,8 @@ for simpler error testing.
 A typical use of the errxpect package is by importing it into the in the current
 file's file block for easy reference without needing the package name.
 
+    import . "github.com/thediveo/errxpect"
+
 Before, Gomega forced you to write assertions where you are only interested in
 checking the error return value of a multi-return value function:
 
@@ -14,14 +16,15 @@ checking the error return value of a multi-return value function:
     _, _, err := multifoo()
     Expect(err).To(Succeed())
 
-This can now be rewritten in a more concise form as:
+This can now be rewritten in a more concise form as (something which isn't
+allowed in stock Gomega Expect-ations):
 
-    Errxpect(multifoo()).To(Succeed())
+    Errxpect(multifoo()).To(HaveOccured())
 
 ExpectWithError() needs to be slightly phrased differently, due to Golang's
 language restrictions, but probably even more neat now:
 
-    Errpect(multifoo()).WithOffset(1).To(Succeed())
+    Errxpect(multifoo()).WithOffset(1).To(Succeed())
 
 */
 package errxpect
